c71ff2e66122226969161b2215a41041cd70d0ad
zhangyuancai <2074497044@qq.com>
2021-05-10 03:09:40 +0800

    第一次提交

---
+++ src/main/java/com/zhang/git/GitApplication.java
@@ -0,0 +1,15 @@
+package com.zhang.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitApplication.class, args);
+        System.out.println("第一次提交");
+    }
+
+}
