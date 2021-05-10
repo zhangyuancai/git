930187535727982d65a953263bb1f1e11fcf4b25
zhangyuancai <2074497044@qq.com>
2021-05-10 10:47:20 +0800

    hot-fix第一次提交

--- src/main/java/com/zhang/git/GitApplication.java
+++ src/main/java/com/zhang/git/GitApplication.java
@@ -12,6 +12,7 @@ public class GitApplication {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
         System.out.println("第三次提交");
+        System.out.println("hot-fix的第一次修改");
     }
 
 }
